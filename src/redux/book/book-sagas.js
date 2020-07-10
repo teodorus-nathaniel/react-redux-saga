194bb03df631074c8d431125a22dd82ce3890f37
Teodorus Nathaniel <53143942+teodorus-nathaniel@users.noreply.github.com>
2020-07-10 08:34:18 +0700

    Update book-sagas.js

--- src/redux/book/book-sagas.js
+++ src/redux/book/book-sagas.js
@@ -1,3 +1,8 @@
+import { all, call, takeLatest, put } from 'redux-saga/effects';
+import createFetchSagaPattern from '../fetch/createFetchSagaPattern';
+import createFetchFunction from '../fetch/createFetchFunction';
+import { loadBook } from './book-actions';
+
 function* fetchBookWorker(data) {
   const id = data.payload.data.id
   console.log(id)
