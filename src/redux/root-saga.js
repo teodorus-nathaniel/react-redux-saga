70f4b948428d55dc4469ac56ea7e2b9d75c0eac8
Teodorus Nathaniel <53143942+teodorus-nathaniel@users.noreply.github.com>
2020-07-10 08:35:18 +0700

    Update root-saga.js

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,6 +1,7 @@
 import { all, call } from 'redux-saga/effects';
 import productSagas from './product/product-sagas';
+import bookSagas from './book/book-sagas';
 
 export default function* rootSaga (){
-  yield all([ call(productSagas) ]);
+  yield all([ call(productSagas), call(bookSagas) ]);
 }
